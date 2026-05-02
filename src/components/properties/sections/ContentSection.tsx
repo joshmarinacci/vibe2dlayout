3c2aa432438008fde8455c420b16077fda64ed9e
joshmarinacci <joshua@marinacci.org>
2026-05-02 12:49:09 -0700

    make the content text area span the full width of the prop sheet

--- src/components/properties/sections/ContentSection.tsx
+++ src/components/properties/sections/ContentSection.tsx
@@ -1,7 +1,6 @@
 import type {AppAction} from '@store/types'
 import type {Dispatch} from 'react'
 import {CollapsibleSection} from '../CollapsibleSection'
-import inputStyles from '../inputs/inputs.module.css'
 
 interface Props {
     id: string
@@ -13,7 +12,7 @@ export function ContentSection({id, content, dispatch}: Props) {
     return (
         <CollapsibleSection title="Content">
 <textarea
-    className={inputStyles.contentTextarea}
+    className={'span-full-grid'}
     value={content}
     rows={3}
     onChange={e =>
